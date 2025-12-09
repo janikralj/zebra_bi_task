6ff97a379552cd280b64fe500ef3cae3ff8470f1
jani <jani.kral@gmail.com>
2025-12-09 12:02:25 +0100

    Minor comment change

--- tests/fixture/fixture.ts
+++ tests/fixture/fixture.ts
@@ -1,11 +1,9 @@
-// fixtures.ts
 import { test as base } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { NavigationBarPage } from '../pages/NavigationBarPage';
 import { ProTrialPage } from '../pages/ProTrialPage';
 import { PricingPage } from '../pages/PricingPage';
 
-// Define the type for the shared object
 type MyFixtures = {
   homePage: HomePage;
   navigationBarPage: NavigationBarPage;
@@ -13,17 +11,17 @@ type MyFixtures = {
   pricingPage: PricingPage;
 };
 
-// Extend the base test with your new fixture
 export const test = base.extend<MyFixtures>({
   homePage: async ({ page }, use) => {
+    // optional: setup logic here
     await use(new HomePage(page));
+    // optional: teardown logic here
   },
   navigationBarPage: async ({ page }, use) => {
     await use(new NavigationBarPage(page));
   },
   proTrialPage: async ({ page }, use) => {
     await use(new ProTrialPage(page));
-    // Optional: Add any teardown logic here
   },
   pricingPage: async ({ page }, use) => {
     await use(new PricingPage(page));
